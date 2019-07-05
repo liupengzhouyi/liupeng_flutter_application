06fc5299466a70c1911a6a4a873d02936e123e0c
liupeng <LIUPENG.0@outlook.com>
2019-07-05 21:08:35 +0800

    Flutter 学习 V 1.0.1 —— 手势处理[点击，双击，长按]

---
+++ lib/dome04.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'Flutter教程',
+    home: new MyButton(),
+  ));
+}
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    // GestureDetector控件没有图像展示，只是检测用户输入的手势
+    return new GestureDetector(
+
+      // 当用户点击Container时，GestureDetector会调用onTap回调
+      onTap: () {
+        print('MyButton被监听了！');
+      },
+
+      // 发生双击时间后回调
+      onDoubleTap: () {
+        print("双击");
+      },
+
+      // 长按事件
+      onLongPress: (){
+        print("长按事件");
+      },
+
+
+      // 当用户点击Container时，GestureDetector会调用onTap回调
+      child: new Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: new BoxDecoration(
+          borderRadius: new BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: new Center(
+          child: new Text('点击监听'),
+        ),
+      ),
+
+    );
+  }
+}
