ade014a576d54a3c8deefb48a62ead2f9fc4e88b
liupeng <37412816+liupengzhouyi@users.noreply.github.com>
2019-07-16 11:02:22 +0800

    Flutter 学习 V 1.0.9 容器[加注释]

--- lib/dome10.dart
+++ lib/dome10.dart
@@ -6,12 +6,19 @@ import 'package:flutter/material.dart';
 class ContainerDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //Center控件使子控件在其内部水平和垂直居中
     return new Center (
       // 这是一个容器控件
+      // 无状态控件
+      //
       child: new Container(
+        // Container控件通过width属性设置宽度为128。
         width: 128.0,
+        // Container控件通过height属性设置高度为128。
         height: 128.0,
+        // 通过decoration属性创建一个BoxDecoration对象，BoxDecoration对象描述如何绘制容器
         decoration: new BoxDecoration(
+          // BoxDecoration对象通过backgroundColor属性为容器设置背景颜色。
           color: Colors.lightBlueAccent[100],
         ),
       ),
